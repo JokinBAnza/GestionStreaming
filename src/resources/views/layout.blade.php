b722652671a54b5d41030076918dcc816ea70c9a
Jokin Berridi <ikdgg@plaiaundi.com>
2025-12-02 12:30:56 +0100

    commit inicial proyecto streaming

---
+++ src/resources/views/layout.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catalogo Streaming</title>
+    <link rel="stylesheet" href="{{ asset('layout.css') }}">
+</head>
+
+<body>
+    <header>
+        <h1>MediaRoll</h1>
+        <nav>
+    <ul>
+        <li><a href="{{ route('categories.index') }}">Media</a></li>
+        <li><a href="{{ route('proveedors.index') }}">Géneros</a></li>
+        <li><a href="{{ route('categories.index') }}">Directores</a></li>
+
+    </ul>
+
+    <!-- Spacer empuja el usuario a la derecha -->
+    <div class="spacer"></div>
+
+    <a id="nombreUsuario" href="{{ route('perfil.index') }}" class="user-link">
+        {{ $Usuario->nombre ?? 'Usuario' }}
+    </a>
+</nav>
+
+        <hr>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <hr>
+        <p>Proyecto CRUD Laravel</p>
+    </footer>
+</body>
+</html>
